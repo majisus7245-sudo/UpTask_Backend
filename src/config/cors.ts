637b06d7f6a43a4dcbd09e2b0b7a42045684e69b
Manuel de Jesus Inzunza Lara <majisus7245@gmail.com>
2026-07-15 02:34:30 -0700

    quitar console.log de puebas

--- src/config/cors.ts
+++ src/config/cors.ts
@@ -3,9 +3,7 @@ import { printColor } from '../helpers'
 
 export const corsConfig: CorsOptions = {
     origin: function(origin, callback){
-        console.log("Origen " + origin);
         const whiteList = [process.env.FRONTEND_URL]
-        console.log("WhiteL " + whiteList);
         if(process.argv[2] === '--api')
             whiteList.push(undefined)
         if(whiteList.includes(origin)){
