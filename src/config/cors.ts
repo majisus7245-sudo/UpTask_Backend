87c5109982f7cda3127cd07500fca9105a25f778
Manuel de Jesus Inzunza Lara <majisus7245@gmail.com>
2026-07-15 02:26:35 -0700

    nuevo

--- src/config/cors.ts
+++ src/config/cors.ts
@@ -3,8 +3,9 @@ import { printColor } from '../helpers'
 
 export const corsConfig: CorsOptions = {
     origin: function(origin, callback){
-        console.log(origin);
+        console.log("Origen " + origin);
         const whiteList = [process.env.FRONTEND_URL]
+        console.log("WhiteL " + whiteList);
         if(process.argv[2] === '--api')
             whiteList.push(undefined)
         if(whiteList.includes(origin)){
